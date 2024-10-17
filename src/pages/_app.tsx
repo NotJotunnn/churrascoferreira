9d1d9f53040776f418029654e3704e7e3c6d64da
Leandro Ferreira <leandro.fcode@gmail.com>
2024-10-17 15:44:38 -0300

    fix SubFooter folder route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 import SuperHeader from "../components/superHeader";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import SubFooter from "../SubFooter";
+import SubFooter from "../components/SubFooter";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
